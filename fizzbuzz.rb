b24bf18c6d30a96c2027c092cabb948da6267214
Justin Daniel <justinddaniel@gmail.com>
2017-09-08 11:39:18 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,4 @@
 def fizzbuzz(x)
-  if x/15 == object.class(Fixnum)
-    puts "FizzBuzz"
-  elsif x/3 == object.class(Fixnum)
-    puts "Fizz"
-  elsif x/5 == object.class(Fixnum)
-    puts "Buzz"
+
   else return nil
 end
